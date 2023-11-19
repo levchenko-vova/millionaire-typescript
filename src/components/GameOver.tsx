2c0ca0e35cc84093dcaa9ed505c13f8365183fc4
Vladimir Levchenko <levchenko.vova@gmail.com>
2023-11-19 16:32:25 +0200

    Fix Result Page

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -8,15 +8,17 @@ interface GameOverProps {
 export default function GameOver({ totalScore }: GameOverProps) {
   return (
     <div className="l-welcomePage">
-      <div className="b-startGame">
-        <div className="b-startGame-column">
-          <img src={welcomeImage} className="b-startGame-image" alt="logo" />
+      <div className="b-gameOver">
+        <div className="b-gameOver-column m-image">
+          <img src={welcomeImage} className="b-gameOver-image" alt="logo" />
         </div>
-        <div className="b-startGame-column">
-          <h4>Total score:</h4>
-          <h1>{totalScore} earned</h1>
-          <div className="b-startGame-actions">
-            <Link to="/" className="b-startGame-startButton b-button">
+        <div className="b-gameOver-column m-text">
+          <div className="b-gameOver-text">
+            <h4>Total score:</h4>
+            <h1>{totalScore} earned</h1>
+          </div>
+          <div className="b-gameOver-actions">
+            <Link to="/" className="b-gameOver-startButton b-button m-sm-100">
               Try Again
             </Link>
           </div>
